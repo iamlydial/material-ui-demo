1a9bbe6f0f499432f6b429ebf7ac7ae20ff72bb5
iamlydial <lydia.lavecchia@gmail.com>
2024-01-15 20:57:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+
+import PhotoCamera from "@material-ui/icons";
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <CssBaseline/>
+        
+      </>
+    </div>
+  );
+};
+
+export default App;
