c7431b063407758aa0d63f5ebb47bd79f631416b
iamlydial <lydia.lavecchia@gmail.com>
2024-01-15 22:45:26 +0100

    grid layout started

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,52 @@ import {
   Grid,
   Toolbar,
   Container,
+  Button,
 } from "@material-ui/core";
 
-import PhotoCamera from "@material-ui/icons";
+import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
   return (
     <div>
       <>
-        <CssBaseline/>
-        
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <PhotoCamera />
+            <Typography variant="h6">Photo Album</Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div>
+            <Container maxWidth="sm">
+              <Typography
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Photo Album
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="textSecondary"
+                paragraph
+              >
+                Hello everyone I am practicing Material UI and I have coded from
+                scratch this photo album.
+              </Typography>
+              <div>
+                <Grid container spacing={2} justifyContent="center">
+                  <Grid item>
+                    <Button variant="contained">See my photos</Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </div>
+        </main>
       </>
     </div>
   );
