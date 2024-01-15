a60d3ee1e1c6a6c0977f2013fdaa4b446f3f89a0
iamlydial <lydia.lavecchia@gmail.com>
2024-01-15 23:20:41 +0100

    added cards layout

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,35 @@ const App = () => {
               </div>
             </Container>
           </div>
+          <Container className={classes.cardGrid} maxWidth="md">
+            <Grid container spacing={4}>
+              <Grid item>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image Title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media Card. This is a description Section
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="secondary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            </Grid>
+          </Container>
         </main>
       </>
     </div>
--- src/styles.js
+++ src/styles.js
@@ -10,6 +10,18 @@ const useStyles = makeStyles((theme) => ({
   buttons: {
     marginTop: "40px",
   },
+  cardGrid: {
+    padding: "20px 0",
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%",
+  },
+  cardContent: { flexGrow: 1 },
 }));
 
 export default useStyles;
